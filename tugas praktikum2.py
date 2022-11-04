a29ea271ebdaa3013facb780638de93550b0d3c8
Juni Saragih <nelpisaragih2306@gmail.com>
2022-11-04 16:53:36 +0700

    Belajar_Python 1

---
+++ tugas praktikum2.py
@@ -0,0 +1,14 @@
+def main():
+    nilai = float(input("Masukkan niali: "))
+
+    if nilai >= 500:
+        print("A")
+    elif nilai >= 450:
+        print("B")
+    elif nilai >= 400:
+        print("C")
+    elif nilai >= 350:
+        print("D")
+    else:
+        print("E")
+main()
